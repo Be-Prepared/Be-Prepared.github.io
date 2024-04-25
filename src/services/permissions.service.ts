8af1902c14fbceb4edf739401ddb0019efd6f5ed
Tyler Akins <fidian@rumkin.com>
2024-04-25 18:12:11 -0500

    Debugging for iOS

--- src/services/permissions.service.ts
+++ src/services/permissions.service.ts
@@ -52,11 +52,14 @@ export class PermissionsService {
 
         const name: PermissionName = 'geolocation';
         const subject = this.#getSubject(name);
+        console.log('1getting geolocation', prompt);
 
         if (prompt) {
             navigator.geolocation.getCurrentPosition(
-                () => subject.next(PermissionsServiceState.GRANTED),
-                () => subject.next(PermissionsServiceState.ERROR),
+                () => {
+                    console.log('1ok');
+                    subject.next(PermissionsServiceState.GRANTED)},
+                () => {console.log('1err'); subject.next(PermissionsServiceState.ERROR)},
                 {
                     timeout: 4000,
                 }
@@ -87,12 +90,15 @@ export class PermissionsService {
 
     #checkPermission(name: PermissionName) {
         const subject = new ReplaySubject<PermissionState>(1);
+        console.log('3check', name);
 
         navigator.permissions.query({ name }).then(
             (status) => {
+                console.log('3status', status.state);
                 subject.next(status.state);
                 // Note: iOS has poor support for onchange.
                 status.onchange = () => {
+                    console.log('3onchange', status.state);
                     subject.next(status.state);
                 };
             },
@@ -109,17 +115,21 @@ export class PermissionsService {
         let observable = this.#observables.get(name);
 
         if (observable) {
+            console.log('2observable');
             return observable;
         }
 
         observable = subject.asObservable().pipe(
             switchMap((value) => {
+                console.log('2value', value);
                 if (value !== null) {
                     return of(value);
                 }
 
                 return this.#checkPermission(name).pipe(
-                    map((state: PermissionState) => this.#mapPermission(state))
+                    map((state: PermissionState) => {
+                        console.log('2state', state);
+                        return this.#mapPermission(state);})
                 );
             }),
             distinctUntilChanged(),
