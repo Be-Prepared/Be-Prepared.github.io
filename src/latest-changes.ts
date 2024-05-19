929f2c34b0209339a402728e05dd6da989403149
Tyler Akins <fidian@rumkin.com>
2024-05-19 09:26:14 -0500

    No longer visible

--- src/latest-changes.ts
+++
@@ -1,7 +0,0 @@
-export const latestChanges = [
-    "URLs are now clickable.",
-    "Improved coordinate parsing and conversion.",
-    "Added NFC tag reading.",
-    "Some devices had the compass east/west reversed.",
-    "Knows locations of 10,000 major cities.",
-]
