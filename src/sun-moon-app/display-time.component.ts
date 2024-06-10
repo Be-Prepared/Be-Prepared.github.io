9e23d8bb078dd62ebbbd510514150dd7f20908e5
Tyler Akins <fidian@rumkin.com>
2024-06-10 17:19:39 -0500

    Honor 24-hour time format
    Closes #42

--- src/sun-moon-app/display-time.component.ts
+++ src/sun-moon-app/display-time.component.ts
@@ -1,4 +1,6 @@
-import { Component, css, html } from 'fudgel';
+import { Component, css, di, html } from 'fudgel';
+import { Subscription } from 'rxjs';
+import { TimeService } from '../services/time.service';
 
 @Component('display-time', {
     attr: ['id', 'failId'],
@@ -7,25 +9,50 @@ import { Component, css, html } from 'fudgel';
     template: html`
         <div *if="time">
             <i18n-label id="{{id}}"></i18n-label>
-            {{timeStr}}
+            <changeable-setting @click.stop.prevent="toggle()">
+                {{timeStr}}
+            </changeable-setting>
         </div>
     `,
 })
 export class DisplayTimeComponent {
+    private _subscription?: Subscription;
+    private _timeService = di(TimeService);
     time?: Date;
     timeStr: string = '';
 
+    onInit() {
+        this._subscription = this._timeService.getCurrentSetting().subscribe(() => {
+            this._redraw();
+        });
+    }
+
+    onDestroy() {
+        this._subscription && this._subscription.unsubscribe();
+    }
+
     onChange(prop: string) {
         if (prop === 'time') {
-            if (
-                this.time &&
-                this.time instanceof Date &&
-                !isNaN(this.time.getTime())
-            ) {
-                this.timeStr = this.time.toLocaleTimeString();
-            } else {
-                this.timeStr = '';
-            }
+            this._redraw();
+        }
+    }
+
+    toggle() {
+        this._timeService.toggleSystem();
+        this._redraw();
+    }
+
+    _redraw() {
+        if (
+            this.time &&
+            this.time instanceof Date &&
+            !isNaN(this.time.getTime())
+        ) {
+            this.timeStr = this._timeService.formatTimeOfDay(
+                this.time.getTime()
+            );
+        } else {
+            this.timeStr = '';
         }
     }
 }
