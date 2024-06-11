7f1e0483d975799c053359ddce70cac60926fb6e
Tyler Akins <fidian@rumkin.com>
2024-06-11 17:39:38 -0500

    Only show one drawer at a time
    Closes #41

--- src/shared/bottom-drawer.component.ts
+++ src/shared/bottom-drawer.component.ts
@@ -1,7 +1,8 @@
 import { Component, css, html, metadataControllerElement } from 'fudgel';
 
+let lock = Promise.resolve();
+
 @Component('bottom-drawer', {
-    prop: ['hide', 'show'],
     style: css`
         :host {
             top: 200vh;
@@ -32,37 +33,53 @@ import { Component, css, html, metadataControllerElement } from 'fudgel';
     useShadow: true,
 })
 export class BottomDrawerComponent {
-    hide?: () => void;
-    show?: () => void;
+    private _lockRelease = () => {};
     private _timeout?: ReturnType<typeof setTimeout>;
 
     onInit() {
-        const hide = (this.hide = () => {
-            if (this._timeout) {
-                clearTimeout(this._timeout);
-            }
-
-            this._element().style.bottom = `-${this._height()}px`;
-            this._timeout = setTimeout(() => this._destroy(), 2000);
-        });
-        this.show = () => {
-            const style = this._element().style;
-            style.top = 'auto';
-            style.bottom = `-${this._height()}px`;
-            this._timeout = setTimeout(() => {
-                style.bottom = '0';
-                this._timeout = setTimeout(() => hide(), 15000);
-            }, 400);
-        };
+        const element = this._element() as any;
+        element.show = () => this._show();
+        element.hide = () => this._hide();
     }
 
     onDestroy() {
         if (this._timeout) {
             clearTimeout(this._timeout);
         }
+
+        if (this._lockRelease) {
+            this._lockRelease();
+        }
+    }
+
+    private _hide() {
+        if (this._timeout) {
+            clearTimeout(this._timeout);
+        }
+
+        this._element().style.bottom = `-${this._height()}px`;
+        this._timeout = setTimeout(() => this._destroy(), 2000);
+    }
+
+    private _show() {
+        lock = lock.then(() => {
+            return new Promise((resolve) => {
+                this._lockRelease = resolve;
+                const style = this._element().style;
+                style.top = 'auto';
+                style.bottom = `-${this._height()}px`;
+                this._timeout = setTimeout(() => {
+                    style.bottom = '0';
+                    this._timeout = setTimeout(() => {
+                        this._hide();
+                    }, 15000);
+                }, 400);
+            })
+        });
     }
 
     private _destroy() {
+        this._lockRelease();
         this._element().remove();
     }
 
